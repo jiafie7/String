b96b03e410b82fdaa52774964b31a12c9989a9b5
jiafie <67643874+jiafie7@users.noreply.github.com>
2024-11-22 20:43:06 +0800

    Commit String

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "utility/string.h"
+
+using namespace melon::utility;
+
+int main()
+{
+  std::cout << String::toLower("AbCDDDD") << '\n';
+  std::cout << String::toUpper("aBcdddd") << '\n';
+
+  std::cout << String::leftTrim("ABC", "BA") << '\n';
+  std::cout << String::rightTrim("hello, world", "ldr") << '\n';
+  std::cout << String::trim("hello, world", "ldhr") << '\n';
+
+  std::string path = "/root/home/dev/index.html";
+  std::vector<std::string> v = String::split(path, "/");
+  for (std::string& s : v)
+    std::cout << s << ' ';
+  std::cout << '\n';
+
+  std::cout << String::join(v, "/") << '\n';
+
+  std::cout << std::boolalpha;
+  std::cout << String::hasPrefix("abcdefg", "abc") << '\n';
+  std::cout << String::hasSuffix("abcdefg", "efg") << '\n';
+  std::cout << std::noboolalpha;
+
+  std::cout << String::capitalize("abc") << '\n';  
+  
+  std::cout << String::format("I am %s, %d years old.\n", "Michael", 17);
+
+  return 0;
+}
